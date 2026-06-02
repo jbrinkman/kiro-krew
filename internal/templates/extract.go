08d33489145c2fb3d2deed374c91369e6310243b
Joseph Brinkman <joe.brinkman@improving.com>
2026-06-02 15:59:40 -0400

    feat: Migrate CLI from switch-based implementation to Cobra framework

---
+++ internal/templates/extract.go
@@ -0,0 +1,56 @@
+package templates
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+var Templates embed.FS
+
+func SetTemplates(templates embed.FS) {
+	Templates = templates
+}
+
+func Extract(srcDir, destDir string, force bool) error {
+	return fs.WalkDir(Templates, srcDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(srcDir, path)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(destDir, relPath)
+
+		if d.IsDir() {
+			return os.MkdirAll(destPath, 0755)
+		}
+
+		if !force {
+			if _, err := os.Stat(destPath); err == nil {
+				return nil
+			}
+		}
+
+		data, err := fs.ReadFile(Templates, path)
+		if err != nil {
+			return err
+		}
+
+		if err := os.MkdirAll(filepath.Dir(destPath), 0755); err != nil {
+			return err
+		}
+
+		if err := os.WriteFile(destPath, data, 0644); err != nil {
+			return err
+		}
+
+		fmt.Printf("Extracted: %s\n", destPath)
+		return nil
+	})
+}
